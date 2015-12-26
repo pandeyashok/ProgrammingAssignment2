d506ba23c052a1717ba23057a2fbf5fc98d116be
Ashok Pandey <pandey.ashok@gmail.com>
2015-12-26 09:09:55 -0600

    Fixed the error

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,14 @@
 
 ## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
+        inv = NULL
+        set = function(y) {
                 x <<- y
                 inv <<- NULL
         }
-        get <- function() x
-        setinverse <- function(sove) inv <<- solve
-        getinverse <- function() inv
+        get = function() x
+        setinverse = function(inverse) inv <<- inverse
+        getinverse = function() inv
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## been calculated (and the matrix has not changed), then it retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinverse()
+        inv = x$getinverse()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
-        data <- x$get()
-        inv <- solve(data)
+        data = x$get()
+        inv = solve(data)
         x$setinverse(inv)
         inv
 }
